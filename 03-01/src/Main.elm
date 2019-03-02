487409294a8f8c96e0c1a5db0b6eecf21d9c6857
tabatak <hamamoto.kazuto@gmail.com>
2019-03-02 13:56:42 +0900

    liタグ生成を関数化

--- 03-01/src/Main.elm
+++ 03-01/src/Main.elm
@@ -6,10 +6,22 @@ import Html.Attributes exposing (href)
 
 main : Html msg
 main =
-    div []
-        [ h1 [] [ text "Useful Links" ]
-        , ul []
-            [ li [] [ a [ href "https://elm-lang.org" ] [ text "Homepage" ] ]
-            , li [] [ a [ href "https://package.elm-lang.org" ] [ text "Packages" ] ]
-            ]
+    div [] [ header, content ]
+
+
+header : Html msg
+header =
+    h1 [] [ text "Useful Links" ]
+
+
+content : Html msg
+content =
+    ul []
+        [ linkItem "https://elm-lang.org" "Homepage"
+        , linkItem "https://package.elm-lang.org" "Packages"
         ]
+
+
+linkItem : String -> String -> Html msg
+linkItem url text_ =
+    li [] [ a [ href url ] [ text text_ ] ]
